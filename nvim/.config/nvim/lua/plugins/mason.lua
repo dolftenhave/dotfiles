80cbea681909ccc661454965a465f7fcd1ceac62
dolftenhave <dolftenhave@gmail.com>
2026-02-17 10:05:10 +1300

    Added some icon opts to mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,15 @@
 return {
 	{
 		"mason-org/mason.nvim",
-		opts = {},
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					backage_uninstalled = "✗",
+				},
+			},
+		},
 		config = function()
 			local mason = require("mason")
 
