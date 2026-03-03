dc6327610e25f977f75a88dee4ae7e9fa34172ea
Dolf ten Have <dolftenhave@gmail.com>
2026-03-04 00:16:50 +1300

    Added autcmd to open the help window to the right of the current pane

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = "*",
 	command = 'silent! normal! g`"zv',
 })
+
+-- split the help docs horizontal instead of vertical
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	callback = function()
+		vim.cmd("wincmd L")
+	end,
+})
