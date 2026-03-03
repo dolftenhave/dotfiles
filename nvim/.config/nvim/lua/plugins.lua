b14caf3f1f6ff3bba0eaa4e31343a921ff3e1336
Dolf ten Have <dolftenhave@gmail.com>
2026-03-04 00:17:08 +1300

    Added blink.cmp for auto complete

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -16,6 +16,12 @@ vim.pack.add({
 	{src = gh("nvim-telescope/telescope-fzf-native.nvim")},
 	{src = gh("nvim-telescope/telescope-ui-select.nvim")},
 	{src = gh("nvim-telescope/telescope.nvim")},
+
+	-- Blink
+	{src = gh("rafamadriz/friendly-snippets")},
+	{src = gh("saghen/blink.cmp"),
+		version = vim.version.range("1.*"),
+},
 })
 
 -- ===================
@@ -71,12 +77,12 @@ require("treesitter-context").setup({
 -- ===================
 
 -- Build telescope-fzf-native.nvim
-plugins = vim.pack.get({"telescope-fzf-native.nvim"})
+local plugins = vim.pack.get({"telescope-fzf-native.nvim"})
 for _, plugin in ipairs(plugins) do
 	vim.system({"make"}, {cwd = plugin.path})
 end
 
-telescope = require("telescope")
+local telescope = require("telescope")
 
 telescope.setup({
 	defaults = { preview = { treesitter = false, }, },
@@ -94,3 +100,77 @@ vim.keymap.set("n", "<leader>fh", function()
 			end, { desc = "telescope find files inc. hidden" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "telescope live grep" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "telescope recent files" })
+
+-- ===================
+-- Blink.cmp
+-- ===================
+
+require("blink.cmp").setup({
+	enabled = function()
+			return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+		end,
+		keymap = {
+			preset = "default",
+			["<C-u>"] = { "scroll_signature_up", "fallback" },
+			["<C-d>"] = { "scroll_signature_down", "fallback" },
+			--["<Tab>"] = { "accept" },
+		},
+
+		signature = {
+			enabled = true,
+		},
+
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+
+		completion = {
+			-- ensures that the text at the start of the word is taken into account when writing completion.
+			keyword = { range = "full" },
+
+			-- The auto show menu.
+			menu = {
+				border = "rounded",
+				draw = {
+					columns = {
+						{
+							"kind_icon",
+							"label",
+							"label_description",
+							gap = 1,
+						},
+						{
+							"kind",
+							gap = 1,
+						},
+						-- this shows the source of the suggestion. e.g. lsp or snippets this the end
+						{
+							"source_name",
+						},
+					},
+					components = {
+						label = {
+							ellipsis = true,
+						},
+					},
+
+					treesitter = { "lsp" },
+				},
+				--auto_show = function()
+				--	return not in_treesitter_capture("comment")
+				--end,
+			},
+
+			documentation = {
+				window = {border = "rounded"},
+				auto_show = true,
+				treesitter_highlighting = true,
+			},
+		},
+
+		sources = {
+			default = { "lsp", "path", "buffer", "snippets" },
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+})
