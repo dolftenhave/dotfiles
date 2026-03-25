1857b06c5550244f29f077839959dc4d7cf5e75a
dolftenhave <dolftenhave@gmail.com>
2026-03-25 15:09:09 +1300

    swapped lualine to earth

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "earth",
+				theme = "gruvbox",
 				icons_enabled = true,
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
