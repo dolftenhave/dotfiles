ee9a3c7965cee3a9e0ccc13f365897bb98c50371
dolftenhave <dolftenhave@gmail.com>
2026-04-15 17:12:42 +1200

    Moved gruvbox into it's own file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,11 +4,9 @@ vim.g.project_dir = vim.env.HOME .. "/code"
 require ("options")
 require ("keymaps")
 require ("autocmds")
-require ("plugins")
 require ("lsp")
 require ("statusline")
 
 -- Enables the gruvbox theme
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd.colorscheme("gruvbox")
