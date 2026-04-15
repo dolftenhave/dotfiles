7be3a41dcbae3f4b8d70425ea63ff0dfba762345
dolftenhave <dolftenhave@gmail.com>
2026-04-15 17:50:09 +1200

    Added some lsp bindings and removed unused bindings

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,10 +1,3 @@
-vim.keymap.set("n", "<leader>s", ":so ~/.config/nvim/init.lua<CR>", { desc = "Source init.lua" }) -- neovim config.
-vim.keymap.set("n", "<leader>x", ":so %<CR>", { desc = "Runs the current lua file" }) -- Run the current line of lua code in the file.
-vim.keymap.set("n", "<leader>X", ":.lua<CR>", { desc = "Runs the current line of lua" }) -- Run the current lua file
-
-
-vim.keymap.set("n", "<leader>h", ":noh<CR>", { desc = "Turn off highlights" })
-
 -- Copies the path and the line of the current file to the clipboard.
 vim.keymap.set("n", "<Leader>xc", ":call setreg('+', expand('%:.') .. ':' .. line('.'))<CR>",
 	{ silent = true, desc = "Coppies the current path of the file and the line number to the clipboard." })
@@ -12,3 +5,14 @@ vim.keymap.set("n", "<Leader>xf", ":call setreg('+', expand('%:.')<CR>",
 	{ silent = true, desc = "Coppies the path of the current file to the clipboard" })
 -- Goes to the current location in the clipboard.
 vim.keymap.set("n", "<Leader>xo", ":e <C-r>+<CR>", { noremap = true, desc = "Go to location in clipboard" })
+
+vim.keymap.set("n", "<C-n>", ":Ex<CR>", { noremap = true, desc = "Open NetRW" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
+		vim.keymap.set("n", "<leader>gf", function() vim.lsp.buf.format({ async = true }) end,
+			{ buffer = bufnr, desc = "Format Buffer" })
+	end,
+})
