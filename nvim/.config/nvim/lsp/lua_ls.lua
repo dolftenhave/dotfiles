7a4edcdacfb44f7606ea880f552705357de74dce
dolftenhave <dolftenhave@gmail.com>
2026-04-16 13:54:19 +1200

    Auto format

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -3,17 +3,16 @@
 
 ---	@type vim.lsp.Config
 return {
-	cmd = {"lua-language-server" },
+	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
-	root_markers = { { ".luarc.json", ".luarc.jsonc",}, ".git" },
+	root_markers = { { ".luarc.json", ".luarc.jsonc", }, ".git" },
 
 	settings = {
 		Lua = {
 			runtime = {
 				version = "LuaJIT",
 			},
-			diagnostics= {
-				enabled = true,
+			diagnostics = {
 				globals = { "vim" },
 			},
 			workspaces = {
