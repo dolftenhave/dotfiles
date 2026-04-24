0236ff1c3db903c106cd161cfe397483034446bf
dolftenhave <dolftenhave@gmail.com>
2026-04-24 13:08:24 +1200

    Added: A version check to stop vim from breaking if this config is used with an older version of vim.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- version check to stop vim from breaking.
+if vim.version().minor < 12 then
+	vim.notify("This config requires nvim 0.12 to work. If you want to run it anyway, then remove the version check in init.lua in your config files.")
+	return
+end
+
 vim.g.project_dir = vim.env.HOME .. "/code"
 
 -- Setup
